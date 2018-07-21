187831586178c2b6be48eafff77977ea0679c444
d3noob <d3.noob.mail+git@gmail.com>
2018-07-21 14:49:58 +1200

    Added more comments

--- setup-pimetric-full.sh
+++ setup-pimetric-full.sh
@@ -6,7 +6,7 @@
 # 
 # PiMetric
 #
-# 2018-07-20
+# 2018-07-21
 #
 ########################################################################
 
@@ -19,13 +19,12 @@
 # (create a ssh file in the root dir of your sd card before turning on)
 # The root partition has been resized and access is 
 # available via eth0 or WiFi to a dynamic IP
-# Once the scripts are transferred to the Pi (using WinSCP)
-# make the setup script (this one) executable by right clicking on
-# it WinSCP or by typing chmod +x ~/<scriptname>
-# Directories that should be transferred are;
-# core_scripts
-# PiMetric
+# Once the setup-pimetric-full.sh script is transferred to the Pi
+# make it executable by right clicking on
+# it WinSCP or by typing chmod +x ~/setup-pimetric-full.sh
+# 
 # Add your unique details in the set up parameters area below.
+# This will be the HOSTNAME and the location of the log file
 
 
 # exit if not running using sudo
@@ -55,22 +54,22 @@ apt-get upgrade -y &>> $LOGFILE 2>&1
 
 echo "Downloading PiMetric..."
 # Get PiMetric and set permissions
-#
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Downloading PiMetric and setting permissions" >> $LOGFILE 2>&1
 echo >> $LOGFILE 2>&1
 
+# Download the PiMetric files
 cd /srv
 curl -L https://github.com/d3noob/PiMetric/archive/master.zip > pimetric.zip
 
+# Unzip the files
 echo "Decompressing the files..."
-
 unzip pimetric.zip
 mv PiMetric-master PiMetric
 
+# Change the permissions
 echo "Setting the permissions..."
-
 chown -R pi:www-data /srv/PiMetric/
 chmod -R 775 /srv/PiMetric/
 usermod -a -G www-data pi
@@ -79,22 +78,22 @@ chmod 666 /srv/PiMetric/monitoring/monitoring
 # Web Server
 # A web server with Nginx and PHP installed
 
-echo "Starting NGINX install..."
 # install nginx
+echo "Starting NGINX install..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Ngnix install started" >> $LOGFILE 2>&1
 apt-get install nginx -y &>> $LOGFILE 2>&1
 
-echo "Starting PHP install..."
 # install php-fpm
+echo "Starting PHP install..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 PHP install started" >> $LOGFILE 2>&1
 apt-get install php-fpm -y &>> $LOGFILE 2>&1
 
-echo "Setting up the sites file..."
 # Set up the sites file
+echo "Setting up the sites file..."
 sed -i -- 's/index index.html index.htm index.nginx-debian.html;/index index.html index.htm index.php;/g' /etc/nginx/sites-available/default
 sed -i -- 's/root \/var\/www\/html;/root \/srv\/PiMetric\/html;/g' /etc/nginx/sites-available/default
 sed -i "/pass PHP scripts to FastCGI server/ r /dev/stdin" /etc/nginx/sites-available/default <<'EOF'
@@ -105,14 +104,13 @@ sed -i "/pass PHP scripts to FastCGI server/ r /dev/stdin" /etc/nginx/sites-avai
 	}
 EOF
 
-echo "Creating an index.php file..."
 # Create an `index.php` file
+echo "Creating an index.php file..."
 echo "<?php phpinfo(); ?>" > /srv/PiMetric/html/index.php
 
-echo "Restarting Nginx..."
 # Restart Nginx
+echo "Restarting Nginx..."
 /etc/init.d/nginx restart
-
 
 # Checking dncpcd.conf
 SITESCONFIG='
@@ -146,71 +144,71 @@ if [ ! -e /var/run/nginx.pid ]; then
     exit
 fi
 
-echo "Installing SQLite..."
 # install SQLite
+echo "Installing SQLite..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 SQLite install started" >> $LOGFILE 2>&1
 apt-get install sqlite3 php7.0-sqlite3 -y &>> $LOGFILE 2>&1
 
-echo "Installing python3-pip..."
 # install python3-pip
+echo "Installing python3-pip..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 python3-pip install started" >> $LOGFILE 2>&1
 apt-get install python3-pip -y &>> $LOGFILE 2>&1
 
-echo "Installing snmp snmpd snmp-mibs-downloader libsnmp-dev..."
 # install snmp snmpd snmp-mibs-downloader libsnmp-dev
+echo "Installing snmp snmpd snmp-mibs-downloader libsnmp-dev..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 snmp snmpd snmp-mibs-downloader libsnmp-dev install started" >> $LOGFILE 2>&1
 apt-get install snmp snmpd snmp-mibs-downloader libsnmp-dev -y &>> $LOGFILE 2>&1
 
-echo "Installing speedtest-cli..."
 # install speedtest-cli
+echo "Installing speedtest-cli..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 speedtest-cli install started" >> $LOGFILE 2>&1
 apt-get install speedtest-cli -y &>> $LOGFILE 2>&1
 
-echo "Installing pip3 module requests..."
 # install Python Module requests via pip3
+echo "Installing pip3 module requests..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Python Module requests install started" >> $LOGFILE 2>&1
 pip3 install requests &>> $LOGFILE 2>&1
 
-echo "Installing pip3 module mysqlclient..."
 # install Python Module mysqlclient via pip3
+echo "Installing pip3 module mysqlclient..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Python Module mysqlclient install started" >> $LOGFILE 2>&1
 pip3 install mysqlclient &>> $LOGFILE 2>&1
 
-echo "Installing pip3 module lxml..."
 # install Python Module lxml via pip3
+echo "Installing pip3 module lxml..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Python Module lxml install started" >> $LOGFILE 2>&1
 pip3 install lxml &>> $LOGFILE 2>&1
 
-echo "Installing pip3 module cssselect..."
 # install Python Module cssselect via pip3
+echo "Installing pip3 module cssselect..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Python Module cssselect install started" >> $LOGFILE 2>&1
 pip3 install cssselect &>> $LOGFILE 2>&1
 
-echo "Installing pip3 module python3-netsnmp..."
 # install Python Module python3-netsnmp via pip3
+echo "Installing pip3 module python3-netsnmp..."
 echo >> $LOGFILE 2>&1
 echo "$(date "+%Y-%m-%d %T") : \
 Python Module python3-netsnmp install started" >> $LOGFILE 2>&1
 pip3 install python3-netsnmp &>> $LOGFILE 2>&1
 
-echo "Setting Hostname..."
 # Therefore to set the hostname
+echo "Setting Hostname..."
 raspi-config nonint do_hostname $HOSTNAME
 
 # Adding the crontab lines
