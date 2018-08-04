2cf63c501572022695606bf42c95d2afae8daec7
d3noob <d3.noob.mail+git@gmail.com>
2018-08-05 10:04:39 +1200

    Added PNG export button to info page
    Required adding 'canvas-to-Blob.js' and 'FileSaver.min.js'  libraries to the project. Based on http://bl.ocks.org/Rokotyan/0556f8facbaf344507cdc45dc3622177

--- html/info.php
+++ html/info.php
@@ -91,21 +91,15 @@ if ($_GET) {
     <!-- jquery dependency -->
     <script src="js/jquery-2.1.4.min.js"></script>
 
+    <!-- Scripts for saving the graph -->
+	<script src="js/canvas-toBlob.js"></script>
+	<script src="js/FileSaver.min.js"></script>
+
 	<style>
 		.centertext {text-align: center;}
 		.righttext {text-align: right;}
 		body { font: 12px Arial;}
-		.axis text{
-			font: 12px sans-serif;
-			fill: #A6A5A5;
-		}
-		.axis path,
-		.axis line {
-			fill: none;
-			stroke: #A6A5A5;
-			stroke-width: 1;
-			shape-rendering: crispEdges;
-		}
+
 		.line {
 			stroke: steelblue;
 			stroke-width: 2;
@@ -150,6 +144,11 @@ if ($_GET) {
 
 	<div class="col-xs-3"> <!-- Left Colunm -->
 		<div class="container-fluid   pull-right" style="width: 100%; margin-top:40px;" >
+
+			<div class="row" style="width: 100%; margin-bottom: 20px;">
+			  <button id='saveButton' class='btn btn-success' style='width: 100%'>Export Graph Image</button>
+			</div>
+
 			<div class="row" style="width: 100%; margin-bottom: 10px">
 				<a href='create.php' class='btn btn-success' style='width: 100%'>Create New Metric</a>
 			</div>
@@ -295,7 +294,6 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
        .attr("height", y(normalLower) - y(normalUpper))	
        .attr("width", width);
 
-
     g.append("line")
        .style("stroke", midblue) // blue
        .attr("x1", 0)				
@@ -368,14 +366,38 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
        .style("shape-rendering", "crispEdges")
        .style("stroke-width", "1px");
 
-    g.append("g")
-        .attr("class", "axis axis--x")
+	var xAxis = g.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
+	xAxis.selectAll("text")
+		.style("font", "12px sans-serif")
+		.style("fill", "#A6A5A5");
+	xAxis.selectAll("line")
+		.style("fill", "none")
+		.style("stroke", "#A6A5A5")
+		.style("stroke-width", 1)
+		.style("shape-rendering", "crispEdges");
+	xAxis.selectAll("path")
+		.style("fill", "none")
+		.style("stroke", "#A6A5A5")
+		.style("stroke-width", 1)
+		.style("shape-rendering", "crispEdges");
 
-    g.append("g")
-        .attr("class", "axis axis--y")
+	var yAxis = g.append("g")
         .call(d3.axisLeft(y).ticks(6));
+	yAxis.selectAll("text")
+		.style("font", "12px sans-serif")
+		.style("fill", "#A6A5A5");
+	yAxis.selectAll("line")
+		.style("fill", "none")
+		.style("stroke", "#A6A5A5")
+		.style("stroke-width", 1)
+		.style("shape-rendering", "crispEdges");
+	yAxis.selectAll("path")
+		.style("fill", "none")
+		.style("stroke", "#A6A5A5")
+		.style("stroke-width", 1)
+		.style("shape-rendering", "crispEdges");
 
     g.append("path")
         .datum(data)
@@ -387,9 +409,8 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
        .attr("y", -10 )
        .style("fill", white)
        .attr("text-anchor", "middle")	
-       .style("font-size", "20px") 
+       .style("font", "20px sans-serif") 
        .text(<?php echo '"'.$label.'"' ?>);
-
 
     var focus = g.append("g")
         .attr("class", "focus")
@@ -472,6 +493,114 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
       focus.select(".y-hover-line").attr("x2", -x(d.date));
     }
 });
+
+// Set-up the export button
+d3.select('#saveButton').on('click', function(){
+	var svgString = getSVGString(svg.node());
+	svgString2Image( svgString, 2*width, 2*height, 'png', save ); // passes Blob and filesize String to the callback
+
+	function save( dataBlob, filesize ){
+		saveAs( dataBlob, 'D3 vis exported to PNG.png' ); // FileSaver.js function
+	}
+});
+
+// Below are the functions that handle actual exporting:
+// getSVGString ( svgNode ) and svgString2Image( svgString, width, height, format, callback )
+function getSVGString( svgNode ) {
+	svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+	var cssStyleText = getCSSStyles( svgNode );
+	appendCSS( cssStyleText, svgNode );
+
+	var serializer = new XMLSerializer();
+	var svgString = serializer.serializeToString(svgNode);
+	svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
+	svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
+
+	return svgString;
+
+	function getCSSStyles( parentElement ) {
+		var selectorTextArr = [];
+
+		// Add Parent element Id and Classes to the list
+		selectorTextArr.push( '#'+parentElement.id );
+		for (var c = 0; c < parentElement.classList.length; c++)
+				if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+					selectorTextArr.push( '.'+parentElement.classList[c] );
+
+		// Add Children element Ids and Classes to the list
+		var nodes = parentElement.getElementsByTagName("*");
+		for (var i = 0; i < nodes.length; i++) {
+			var id = nodes[i].id;
+			if ( !contains('#'+id, selectorTextArr) )
+				selectorTextArr.push( '#'+id );
+
+			var classes = nodes[i].classList;
+			for (var c = 0; c < classes.length; c++)
+				if ( !contains('.'+classes[c], selectorTextArr) )
+					selectorTextArr.push( '.'+classes[c] );
+		}
+
+		// Extract CSS Rules
+		var extractedCSSText = "";
+		for (var i = 0; i < document.styleSheets.length; i++) {
+			var s = document.styleSheets[i];
+			
+			try {
+			    if(!s.cssRules) continue;
+			} catch( e ) {
+		    		if(e.name !== 'SecurityError') throw e; // for Firefox
+		    		continue;
+		    	}
+
+			var cssRules = s.cssRules;
+			for (var r = 0; r < cssRules.length; r++) {
+				if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+					extractedCSSText += cssRules[r].cssText;
+			}
+		}
+
+		return extractedCSSText;
+
+		function contains(str,arr) {
+			return arr.indexOf( str ) === -1 ? false : true;
+		}
+
+	}
+
+	function appendCSS( cssText, element ) {
+		var styleElement = document.createElement("style");
+		styleElement.setAttribute("type","text/css"); 
+		styleElement.innerHTML = cssText;
+		var refNode = element.hasChildNodes() ? element.children[0] : null;
+		element.insertBefore( styleElement, refNode );
+	}
+}
+
+function svgString2Image( svgString, width, height, format, callback ) {
+	var format = format ? format : 'png';
+
+	var imgsrc = 'data:image/svg+xml;base64,'+ btoa( unescape( encodeURIComponent( svgString ) ) ); // Convert SVG string to data URL
+
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext("2d");
+
+	canvas.width = width;
+	canvas.height = height;
+
+	var image = new Image();
+	image.onload = function() {
+		context.clearRect ( 0, 0, width, height );
+		context.drawImage(image, 0, 0, width, height);
+
+		canvas.toBlob( function(blob) {
+			var filesize = Math.round( blob.length/1024 ) + ' KB';
+			if ( callback ) callback( blob, filesize );
+		});
+
+	};
+
+	image.src = imgsrc;
+}
 
 </script>
 
@@ -623,7 +752,6 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
 
 </div> <!-- Row -->
 
-
     <!-- Another row -->
 
 	<div class="row" >
@@ -640,7 +768,6 @@ d3.json(<?php echo '"'.'data.php?name='.$name.'"'; ?>, function(error, data) {
 		</div> <!-- /centertext -->
 		</div> <!-- /column -->
 	</div> <!-- /row -->
-
 
 </div> <!-- /container   -->
 
